c2b586cd13486678dfb0915d7735c6d4d9dd3d21
Ali Bordbar <witch_king2011@yahoo.com>
2019-02-06 00:23:22 +0330

    created user page for testing user registration

---
+++ OWM.UI.Web/Pages/User.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OWM.Application.Services.Dtos;
+using OWM.Application.Services.Interfaces;
+using OWM.Domain.Entities;
+using OWM.Domain.Services;
+using OWM.Domain.Services.Interfaces;
+using OWM.UI.Web.Dtos;
+using URF.Core.Abstractions;
+
+namespace OWM.UI.Web.Pages
+{
+    public class UserModel : PageModel
+    {
+        private readonly IUserRegistrationService _registrationService;
+
+
+        [BindProperty]
+        public UserRegistrationDto MyUser { get; set; }
+
+        public UserModel(IUserRegistrationService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _registrationService.Register(MyUser);
+            return RedirectToPage("/Index");
+        }
+    }
+}
