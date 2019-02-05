dde14344b381413614775072a9f46fe73d7a28ce
IT2 <IT2@IT2>
2019-02-05 14:59:09 +0330

    changes

---
+++ OWM.UI.Web/Pages/user.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OWM.UI.Web.Pages
+{
+    public class userModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OWM.UI.Web/utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OWM.UI.Web
+{
+    public class utils
+    {
+        
+    }
+
+}
