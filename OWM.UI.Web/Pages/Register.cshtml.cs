17fec296520665676df88f3d3050c04dffeef125
IT2 <IT2@IT2>
2019-02-06 13:09:45 +0330

    register page created

--- OWM.UI.Web/Pages/Register.cshtml.cs
+++ OWM.UI.Web/Pages/Register.cshtml.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace OWM.UI.Web.Pages
 {
-    public class userModel : PageModel
+    public class RegisterModel : PageModel
     {
         public void OnGet()
         {
+            
         }
     }
 }
