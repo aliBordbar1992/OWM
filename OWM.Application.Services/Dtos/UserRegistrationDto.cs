8847f546faa507103158cf6404bea057b7b08db4
Omid <Omid@DESKTOP-LJM88T4>
2019-02-08 15:50:10 +0330

    Register Page Created

--- OWM.Application.Services/Dtos/UserRegistrationDto.cs
+++ OWM.Application.Services/Dtos/UserRegistrationDto.cs
@@ -6,20 +6,41 @@ namespace OWM.Application.Services.Dtos
 {
     public class UserRegistrationDto
     {
+        [Required(ErrorMessage = "Occupation is Required")]
         public int OccupationId { get; set; }
+        [Required(ErrorMessage = "City is Required")]
         public int CityId { get; set; }
+        [Required(ErrorMessage = "Ethnicity is Required")]
         public int EthnicityId { get; set; }
+        [Required(ErrorMessage = "Gender is Required")]
         public int Gender { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Surname is Required")]
         public string Surname { get; set; }
+        [Required(ErrorMessage = "Mobile no. is required")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone no.")]
         public string Phone { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Country is required")]
         public string CountryName { get; set; }
         public string CityName { get; set; }
-
-        public DateTime DateOfBirth => DateTime.ParseExact(Birthday, "yyyy/MM/dd", null);
-
+        [Required(ErrorMessage = "Birthday is required")]
+        public DateTime DateOfBirth => DateTime.ParseExact(Birthday, "yyyy-MM-dd", null);
+        [Required(ErrorMessage = "Birthday is required")]
         public string Birthday { get; set; }
     }
 }
---
+++ OWM.UI.Web/wwwroot/js/Register.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    $(".chosen-select").chosen({ width: "100%" });
+    $('input[placeholder=City]').on('keyup', function () {
+        if ($(this).val() == "" || $(this).val().length < 3) {
+            $('ul[name=cityfilter]').empty();
+            return;
+        }
+        setTimeout(
+            function () {
+                jQuery.getJSON(
+                    "http://gd.geobytes.com/AutoCompleteCity?callback=?&sort=size&q=" + $('input[placeholder=City]').val(),
+                    function (data) {
+                        var d = [];
+                        $('ul[name=cityfilter]').empty();
+                        data.forEach(function (item) {
+                            d.push('<li>' + item + '</li>');
+                        });
+                        $('ul[name=cityfilter]').append(d.join(''));
+                    }
+                );
+            }, 1500);
+    });
+    var city;
+    $('ul[name=cityfilter]').on('click', 'li',
+        function () {
+            city = this.innerText;
+            $('input[placeholder=City]').val(city);
+            $('ul[name=cityfilter]').empty();
+            $('input[placeholder=City]').prop('disabled', true);
+            GetCountry(this);
+        });
+
+    function GetCountry(e) {
+        jQuery.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?&fqcn=" + e.innerText,
+            function (data) {
+                $('input[name=country]').val(data.geobytescountry);
+                $('input[name=cityid]').val(data.geobytescityid);
+            }
+        );
+    }
+
+    $('.fa-times').click(function () {
+        $('input[placeholder=City]').val('');
+        $('input[name=country]').val('');
+        $('input[placeholder=City]').prop('disabled', false);
+        $('input[name=cityid]').val('');
+    });
+});
