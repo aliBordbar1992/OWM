2030336c8758af48db6571b82fd8dee1d1f7443e
Ali Bordbar <witch_king2011@yahoo.com>
2019-02-10 16:41:00 +0330

    update registration process to send verification email

--- OWM.UI.Web/Pages/Register.cshtml.cs
+++ OWM.UI.Web/Pages/Register.cshtml.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.DependencyInjection;
 using OWM.Application.Services;
 using OWM.Application.Services.Dtos;
+using OWM.Application.Services.Email;
 using OWM.Application.Services.Interfaces;
 
 namespace OWM.UI.Web.Pages
@@ -15,11 +17,15 @@ namespace OWM.UI.Web.Pages
     public class RegisterModel : PageModel
     {
         private readonly IUserRegistrationService _userRegistrationService;
+        private readonly IUserVerificationService _userVerificationService;
+        private readonly IServiceProvider _serviceProvider;
         public List<SelectListItem> EthnicityOptions;
         public List<SelectListItem> OccupationOptions;
-        public RegisterModel(IUserRegistrationService userRegistrationService)
+        public RegisterModel(IServiceProvider serviceProvider)
         {
-             _userRegistrationService = userRegistrationService;
+            _serviceProvider = serviceProvider;
+            _userRegistrationService = serviceProvider.GetRequiredService<IUserRegistrationService>();
+            _userVerificationService = serviceProvider.GetRequiredService<IUserVerificationService>();
              EthnicityOptions = new List<SelectListItem>();
              OccupationOptions = new List<SelectListItem>();
         }
@@ -38,8 +44,10 @@ namespace OWM.UI.Web.Pages
                 return Page();
             }
             FillDropdowns();
-            _userRegistrationService.Register(RegistrationData);
-            return RedirectToPage("/Register");
+            _userRegistrationService.UserRegistered += SendVerificationEmail;
+
+            await _userRegistrationService.Register(RegistrationData);
+            return RedirectToPage("/Verify");
         }
 
         public void FillDropdowns()
@@ -55,6 +63,19 @@ namespace OWM.UI.Web.Pages
                 Text = x.Name,
                 Value = x.Id + ""
             }).ToList().Result;
+        }
+
+        public void SendVerificationEmail(object sender, UserRegisteredArgs e)
+        {
+            if (!e.User.EmailVerified)
+            {
+                var hostingEnv = _serviceProvider.GetRequiredService<IHostingEnvironment>();
+                var verification = _userVerificationService.CreateEmailVerificationCode(e.User.Id).Result;
+
+                string link = TemplateHelper.GetVerifyLink(verification.VerificatonCode.ToString());
+                VerificationEmailSender emailSender = new VerificationEmailSender(hostingEnv, e.User.User.Name, e.User.Email, link);
+                emailSender.Send();
+            }
         }
     }
 }
--- OWM.UI.Web/Startup.cs
+++ OWM.UI.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OWM.Application.Services.AppConfigs;
 using OWM.Data;
+using OWM.Domain.Entities;
 
 namespace OWM.UI.Web
 {
