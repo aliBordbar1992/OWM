1bc3ac01a2f89643505619654e7c65a50b7da3d2
Ali Bordbar <witch_king2011@yahoo.com>
2019-02-17 00:43:54 +0330

    Removed test services

--- OWM.Domain.Services/Interfaces/ITest.cs
+++
@@ -1,9 +0,0 @@
-﻿using OWM.Domain.Entities;
-using URF.Core.Abstractions.Services;
-
-namespace OWM.Domain.Services.Interfaces
-{
-    public interface ITestEntityService : IService<TestEntity>
-    {
-    }
-}
