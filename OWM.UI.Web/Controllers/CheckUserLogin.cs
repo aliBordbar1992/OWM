6756cdbdca9a51bbb0d0fee7c4385729c15aa2a8
IT2 <IT2@IT2>
2019-02-18 14:18:31 +0330

    change some page design from scratch

--- OWM.UI.Web/Controllers/CheckUserLogin.cs
+++ OWM.UI.Web/Controllers/CheckUserLogin.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OWM.Application.Services.Dtos;
+using OWM.Domain.Entities;
 
 namespace OWM.UI.Web.Controllers
 {
