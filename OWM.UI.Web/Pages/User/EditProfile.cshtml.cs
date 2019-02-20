a55494936f4e62a4526ee1c7d8f49cdc5cb2244b
IT2 <IT2@IT2>
2019-02-20 11:41:21 +0330

    admin pages created again

--- OWM.UI.Web/Pages/User/EditProfile.cshtml.cs
+++ OWM.UI.Web/Pages/User/EditProfile.cshtml.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
 using OWM.Application.Services.Interfaces;
 using OWM.Application.Services.Dtos;
+using OWM.Application.Services.Exceptions;
+using OWM.Domain.Entities;
 
 namespace OWM.UI.Web.Pages.User
 {
@@ -14,16 +18,29 @@ namespace OWM.UI.Web.Pages.User
     {
         private readonly IUserRegistrationService _userRegistrationService;
         public List<SelectListItem> EthnicityOptions;
+        private readonly SignInManager<UserIdentity> _signInManager;
+        private readonly IUserInformationService _userInformation;
 
         [BindProperty] public UserInformationDto UserInformationDto { get; set; }
 
-        public EditProfileModel(IServiceProvider serviceProvider)
+        public EditProfileModel(IServiceProvider serviceProvider, SignInManager<UserIdentity> signInManager,
+            IUserInformationService userInformation)
         {
+            _signInManager = signInManager;
+            _userInformation = userInformation;
             _userRegistrationService = serviceProvider.GetRequiredService<IUserRegistrationService>();
             EthnicityOptions = new List<SelectListItem>();
         }
         public void OnGet()
         {
+            if (_signInManager.IsSignedIn((ClaimsPrincipal)User))
+            {
+                string identityId = _signInManager.UserManager.GetUserId((ClaimsPrincipal)User);
+                var info = _userInformation.GetUserInformation(identityId);
+                UserInformationDto = info;
+            }
+            else throw new UserNotFoundException();
+
             EthnicityOptions = _userRegistrationService.GetEthnicities().Select(x => new SelectListItem
             {
                 Text = x.Name,
