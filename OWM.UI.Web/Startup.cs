a60138533d4d392c3ea7f9996fb03e1ae8c82cf7
Ali Bordbar <witch_king2011@yahoo.com>
2019-03-12 20:29:54 +0330

    password selection policy updated

--- OWM.UI.Web/Startup.cs
+++ OWM.UI.Web/Startup.cs
@@ -90,15 +90,15 @@ namespace OWM.UI.Web
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                //options.Password.RequireDigit = true;
-                //options.Password.RequireLowercase = true;
-                //options.Password.RequireNonAlphanumeric = true;
-                //options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
                 options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
+                //options.Password.RequiredUniqueChars = 1;
 
                 // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.AllowedForNewUsers = true;
 
