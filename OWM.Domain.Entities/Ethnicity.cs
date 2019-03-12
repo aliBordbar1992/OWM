4869d8be682547a71f81c9f6adb5fb7ed352c040
Ali Bordbar <witch_king2011@yahoo.com>
2019-03-12 23:43:54 +0330

    order column added

--- OWM.Domain.Entities/Ethnicity.cs
+++ OWM.Domain.Entities/Ethnicity.cs
@@ -6,5 +6,6 @@ namespace OWM.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Order { get; set; }
     }
 }
--- OWM.Domain.Entities/Occupation.cs
+++ OWM.Domain.Entities/Occupation.cs
@@ -11,6 +11,7 @@ namespace OWM.Domain.Entities
         }
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Order { get; set; }
         public ICollection<TeamOccupations> InTeams { get; set; }
     }
 }
