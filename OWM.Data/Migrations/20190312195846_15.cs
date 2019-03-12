2b535133e2e49d218880ea9dadbfea937006020b
Ali Bordbar <witch_king2011@yahoo.com>
2019-03-12 23:44:29 +0330

    migrations

---
+++ OWM.Data/Migrations/20190312195846_15.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OWM.Data.Migrations
+{
+    public partial class _15 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Occupations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Ethnicities",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Occupations");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Ethnicities");
+        }
+    }
+}
