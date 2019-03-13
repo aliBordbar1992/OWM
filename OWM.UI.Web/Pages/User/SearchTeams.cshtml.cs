ed1b649aff9e62b5c62852334eb2cdfd6c0c4681
Ali Bordbar <witch_king2011@yahoo.com>
2019-03-13 21:51:59 +0330

    fixed search teams authoriz bug

---
+++ OWM.UI.Web/Pages/User/SearchTeams.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OWM.UI.Web.Pages.User
+{
+    [Authorize]
+    public class SearchTeamsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
