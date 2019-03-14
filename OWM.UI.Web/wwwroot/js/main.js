b006ed9a776c177bec578832cfb404daf605c866
IT2 <IT2@IT2>
2019-03-14 11:29:01 +0330

    top menu changes

--- OWM.UI.Web/wwwroot/js/main.js
+++ OWM.UI.Web/wwwroot/js/main.js
@@ -17,33 +17,6 @@
         })
     };
 
-    // MMENU
-    var mobileMenu = $('.mobile-menu');
-
-    mobileMenu.mmenu({
-        counters: true,
-        offCanvas: {
-            zposition: 'back',
-            position: 'right'
-        }
-    }, {
-        classNames: {
-            fixedElements: {
-                fixed: 'fix'
-            }
-        }
-    });
-
-    var api = mobileMenu.data('mmenu');
-
-    $('.mobile-open').click(function () {
-        if ($('html').hasClass('mm-opened')) {
-            api.close()
-        } else {
-            api.open()
-        }
-    });
-
     // GO TO PLEDGE
     $('.go-to-pledge').click(function (e) {
         e.preventDefault();
