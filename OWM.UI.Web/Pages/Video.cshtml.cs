c21d3544dee99e389590663cc5e50ae23856b2e5
Ali Bordbar <witch_king2011@yahoo.com>
2019-03-18 23:34:41 +0330

    pic and video complete

--- OWM.UI.Web/Pages/Video.cshtml.cs
+++ OWM.UI.Web/Pages/Video.cshtml.cs
@@ -1,16 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.DotNet.PlatformAbstractions;
 
 namespace OWM.UI.Web.Pages
 {
     public class Video : PageModel
     {
+        private readonly IHostingEnvironment _hostingEnv;
+        public Video(IHostingEnvironment hostingEnv)
+        {
+            _hostingEnv = hostingEnv;
+        }
+
         public void OnGet()
         {
+            videos = new List<VideoFiles>();
+
+            string path = Path.Combine(_hostingEnv.WebRootPath, "video\\gallery\\");
+            
+            foreach (string file in Directory.EnumerateFiles(path, "*", SearchOption.TopDirectoryOnly))
+            {
+                string title = Path.GetFileNameWithoutExtension(file);
+                videos.Add(new VideoFiles
+                {
+                    Title = title,
+                    Address = $"/video/gallery/{Path.GetFileName(file)}"
+                });
+            }
+        }
+
+        public List<VideoFiles> videos { get; set; }
+
+        public class VideoFiles
+        {
+            public string Title { get; set; }
+            public string Address { get; set; }
         }
     }
 }
