f2669d5c03560e148dcd8c1333fb43e5d52add57
Omid <Omid@DESKTOP-LJM88T4>
2019-03-22 02:53:11 +0430

    minor fix

--- OWM.UI.Web/wwwroot/homepage/home.js
+++ OWM.UI.Web/wwwroot/homepage/home.js
@@ -23,4 +23,13 @@
     } else {
         participant.start();
     }
+
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0] - 1, mdy[1]);
+    }
+    function datediff(first, second) {
+        return Math.round((second - first) / (1000 * 60 * 60 * 24));
+    }
+    $('.days').text((datediff(parseDate(new Date().toLocaleDateString()), parseDate('04/12/2019'))));
 });
