e60621fd6409db45ec9581377bfb4db9024cb664
IT2 <IT2@IT2>
2019-03-27 12:32:29 +0430

    minor changes

--- OWM.UI.Web/wwwroot/js/main.js
+++ OWM.UI.Web/wwwroot/js/main.js
@@ -43,7 +43,8 @@
     $('#header, #splash').addClass('anim');
 
     // READ MORE
-    $(document).on('click','.block p',function () {
+    $(document).on('click','.block p', function () {
+        if ($(document).width() > 550) return;
         var block = $(this).closest('.block');
 
         if (!block.hasClass('.anim')) {
