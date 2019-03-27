c50903b35a6c6563bda0e42c12a31ff3383e817c
IT2 <IT2@IT2>
2019-03-27 13:19:41 +0430

    days until the event fixed

--- OWM.UI.Web/wwwroot/homepage/home.js
+++ OWM.UI.Web/wwwroot/homepage/home.js
@@ -23,13 +23,5 @@ $(function () {
     } else {
         participant.start();
     }
-	
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0] - 1, mdy[1]);
-    }
-    function datediff(first, second) {
-        return Math.round((second - first) / (1000 * 60 * 60 * 24));
-    }
-    $('.days').text((datediff(parseDate(new Date().toLocaleDateString()), parseDate('04/12/2019'))));
+    $('.days').text($('#days-until-the-event').val());
 });
